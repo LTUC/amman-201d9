d5a7eb7729e9b5c74daaab565d62ca1fbe5cea94
hanaa maratouk <hanaamaratouk@gmail.com>
2020-09-17 09:47:41 +0300

    git practices

---
+++ class-10/demo/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var arr = ['1', '4', '6', '9', '11','22'];
+
+var x = 5;
+x = x + 4;
+x++;
+var y = x;
+y = y + 9;
+
+console.log(sum(x,y));
+
+
+function sum(num1, num2){
+  var sum = num1 + num2;
+}
